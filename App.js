d12162feff92ab5161a782dc3136dc78b38794db
k644606347 <k644606347@126.com>
2018-05-27 23:08:17 +0800

    main files commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer2 menu.</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
